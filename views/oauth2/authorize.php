038dcb4f000d16eff9a5170c2410dda4b971dd7d
Liv <523260513@qq.com>
2015-02-06 23:43:27 +0800

    Yii OAuth2 Server Module

---
+++ views/oauth2/authorize.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @var Oauth2Controller $this
+ */
+
+?>
+
+<form method="post">
+  <label>Do You Authorize TestClient?</label><br />
+  <input type="submit" name="authorized" value="yes">
+  <input type="submit" name="authorized" value="no">
+</form>
